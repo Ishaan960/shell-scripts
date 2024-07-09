fbed3a6a07c844b8a21d976f0fe877aacf69cf35
Ishaan960 <ishaan.yerriboina.neo@gmail.com>
2024-07-09 23:58:08 +0530

    Update diskusage.sh

--- scenarios/diskusage.sh
+++ scenarios/diskusage.sh
@@ -5,5 +5,4 @@ disk_usage=$(df -h | grep "/dev/sda1" | awk '{print $5}' | tr -d '%')
 
 if [ "$disk_usage" -gt "$threshold" ]; then
     echo "Disk space usage exceeded $threshold%"
-    # Send alert here
 fi
